2139c1466466a3d1a930deb93c5270ab66db999a
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2011-06-06 15:21:22 +0200

    Fix notice

--- application/models/Users.php
+++ application/models/Users.php
@@ -8,7 +8,7 @@ class Application_Model_Users extends Zend_Db_Table_Abstract
     {
         $rows = $this->find($data['user_id']);
         $rows = $rows->toArray();
-        if (!$rows[0]['user_id']==$data['user_id']) {
+        if (isset($rows[0]) && !$rows[0]['user_id']==$data['user_id']) {
             $data['first_login_at'] = new Zend_Db_Expr('NOW()');
             parent::insert($data);
         } else {
