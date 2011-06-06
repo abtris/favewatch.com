d1708baf988159c2908c290503fe2f72368c4d40
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2011-06-06 16:15:12 +0200

    Update db.sql

--- docs/db.sql
+++ docs/db.sql
@@ -1,9 +1,37 @@
+truncate users;
+truncate friends;
+truncate friends_tweets;
+truncate tweets;
+
 -- Adminer 3.3.0-dev MySQL dump
 
 SET NAMES utf8;
 SET foreign_key_checks = 0;
 SET time_zone = 'SYSTEM';
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `friends`;
+CREATE TABLE `friends` (
+  `friend_id` bigint(20) NOT NULL,
+  `screen_name` varchar(255) DEFAULT NULL,
+  `profile_image_url` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `last_update` datetime DEFAULT NULL,
+  PRIMARY KEY (`friend_id`),
+  UNIQUE KEY `friend_id` (`friend_id`,`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `friends_tweets`;
+CREATE TABLE `friends_tweets` (
+  `friend_id` bigint(20) NOT NULL,
+  `tweet_id` bigint(20) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`friend_id`,`tweet_id`),
+  UNIQUE KEY `friend_id` (`friend_id`,`tweet_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 
 DROP TABLE IF EXISTS `tweets`;
 CREATE TABLE `tweets` (
@@ -25,9 +53,8 @@ CREATE TABLE `users` (
   `user_id` bigint(20) NOT NULL,
   `last_login_at` datetime NOT NULL,
   `first_login_at` datetime NOT NULL,
-  `friends` longtext,
   PRIMARY KEY (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
--- 2011-06-04 18:13:40
+-- 2011-06-06 16:09:18
